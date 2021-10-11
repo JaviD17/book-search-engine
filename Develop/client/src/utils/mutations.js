058e74588aeb7c623c65c2195e04dc8a3f8ff719
Javier Dominguez II <Javi7057@gmail.com>
2021-10-10 20:55:47 -0500

    added mutations and queries

---
+++ Develop/client/src/utils/mutations.js
@@ -0,0 +1,18 @@
+// LOGIN_USER loginUser
+// ADD_USER addUser
+// SAVE_BOOK saveBook
+// REMOVE_BOOK removeBook
+
+import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
